df2f7b85232834b7acf1e4442306cd51ceed82cc
Furkan Sertçe <furkansertc@gmail.com>
2018-09-25 23:07:33 +0300

    Update

---
+++ Bilbakalim/Bilbakalim/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bilbakalim.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Quiz
+        public ActionResult Quiz()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Bilbakalim/Bilbakalim/Models/SoruModel.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+
+//namespace Bilbakalim.Models
+//{
+//    public class SoruModel
+//    {
+
+
+//        public Sorular SoruOlustur(Sorular yeniSoru, List<Cevaplar> yeniCevaplar)
+//        {
+
+//            try
+//            {
+//                using (var context = new BilBakalimEntities())
+//                {
+//                    context.Sorular.Add(yeniSoru);
+//                    foreach (var item in yeniCevaplar)
+//                    {
+//                        context.Cevaplar.Add(item);
+//                    }
+//                    context.SaveChanges();
+//                    return yeniSoru;
+//                }
+//            }
+//            catch (Exception)
+//            {
+
+//                throw;
+//            }
+
+
+//        }
+//    }
+//}
